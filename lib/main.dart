0857e044844500151df5f7bb12616e673e4bda5f
prakhar1032 <guptaprakhar1032@gmail.com>
2023-05-27 11:08:47 +0530

    working

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutterfirst/home_page.dart';
+import 'package:flutterfirst/pages/login_page.dart';
+import 'pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      theme: ThemeData(fontFamily: GoogleFonts.lato().fontFamily),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
+    );
   }
 }
